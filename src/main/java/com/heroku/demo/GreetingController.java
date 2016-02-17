5ef1c55d25106cd8107c8f1560e4c507c85bc8ef
phyrionX <rgcmb@hotmail.com>
2016-02-17 02:30:26 +0100

    Probando autodesplegado en HEroku

--- src/main/java/com/heroku/demo/GreetingController.java
+++ src/main/java/com/heroku/demo/GreetingController.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello probando cambios automaticos, %s!";
+    private static final String template = "Hello , %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
