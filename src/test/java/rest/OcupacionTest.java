f36139083795158c414792fdb291fb0fb5fba8f8
Mario <mario94almunia@gmail.com>
2016-05-23 23:33:48 +0200

    Test Ocupacion

---
+++ src/test/java/rest/OcupacionTest.java
@@ -0,0 +1,50 @@
+package rest;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import rest.common.Ocupacion;
+
+import static org.junit.Assert.assertEquals;
+
+public class OcupacionTest {
+
+    private static Ocupacion ocupacion;
+    private static Ocupacion ocupacionVacia;
+    private static Ocupacion ocupacionLlena;
+
+    @BeforeClass
+    public static void beforeClass(){
+        ocupacion = new Ocupacion(60, 30);
+        ocupacionLlena = new Ocupacion(60, 60);
+        ocupacionVacia = new Ocupacion(60,0);
+    }
+
+    @Test
+    public void ocuparEnOcupacionNormal () {
+        int ocupadasAntes = ocupacion.getOcupadas();
+        ocupacion.ocuparPlaza();
+        assertEquals(ocupacion.getOcupadas(),ocupadasAntes+1);
+    }
+
+    @Test
+    public void liberarEnOcupacionNormal () {
+        int ocupadasAntes = ocupacion.getOcupadas();
+        ocupacion.liberarPlaza();
+        assertEquals(ocupacion.getOcupadas(),ocupadasAntes-1);
+    }
+
+    @Test
+    public void liberarEnOcupacionVacia () {
+        int ocupadasAntes = ocupacionVacia.getOcupadas();
+        ocupacionVacia.liberarPlaza();
+        assertEquals(ocupacionVacia.getOcupadas(),ocupadasAntes);
+    }
+
+    @Test
+    public void ocuparEnOcupacionLlena () {
+        int ocupadasAntes = ocupacionLlena.getOcupadas();
+        ocupacionLlena.ocuparPlaza();
+        assertEquals(ocupacionLlena.getOcupadas(),ocupadasAntes);
+    }
+
+}
