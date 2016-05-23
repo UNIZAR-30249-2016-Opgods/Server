93448385b4b72c298f7f7bab5bc2339fee678ba3
phyrion <rgcmb@hotmail.com>
2016-05-23 11:55:51 +0200

    Empezando a añadir cafeterías v2

--- src/main/java/rest/cafeterias/Cafeteria.java
+++ src/main/java/rest/cafeterias/Cafeteria.java
@@ -1,9 +1,51 @@
 package rest.cafeterias;
 
 import rest.common.Entidad;
+import rest.common.Ocupacion;
+import rest.common.Punto;
 
 /**
  * Created by phyrion on 23/05/16.
  */
 public class Cafeteria extends Entidad {
+    private String nombre;
+    private Punto punto;
+    private Ocupacion ocupacion;
+
+    public Cafeteria(String nombre, Punto punto, Ocupacion ocupacion) {
+        this.nombre = nombre;
+        this.punto = punto;
+        this.ocupacion = ocupacion;
+    }
+
+    public Cafeteria(String id, String nombre, Punto punto, Ocupacion ocupacion) {
+        super(id);
+        this.nombre = nombre;
+        this.punto = punto;
+        this.ocupacion = ocupacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Punto getPunto() {
+        return punto;
+    }
+
+    public void setPunto(Punto punto) {
+        this.punto = punto;
+    }
+
+    public Ocupacion getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setOcupacion(Ocupacion ocupacion) {
+        this.ocupacion = ocupacion;
+    }
 }
